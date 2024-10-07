10da26099797b0e91e18cda6f4734ce8ef59c3eb
Tom Hicks <tom.hicks@me.com>
2024-10-07 10:51:24 +0200

    Clean up build output

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,11 @@ import fs from "fs";
 import path from "path";
 import { defineConfig } from "tsup";
 
+const files = fs.readdirSync("./").filter(isCopiedTargetFile);
+for (const file of files) {
+  fs.unlinkSync(path.join(".", file));
+}
+
 export default defineConfig({
   entryPoints: ["src/client.ts", "src/handler.ts"],
   format: ["cjs", "esm"],
@@ -18,15 +23,29 @@ export default defineConfig({
 // move the files to the root for node 10 support (really just to make attw happy)
 // onSuccess cannot be used because it runs before the types are generated.
 process.on("exit", () => {
-  const files = fs.readdirSync("./dist");
+  const files = fs.readdirSync("./dist").filter(isDesiredSourceFile);
   for (const file of files) {
-    if (
-      file.endsWith("handler.d.ts") ||
-      file.endsWith("handler.cjs") ||
-      (file.endsWith(".d.ts") && file.includes("types-"))
-    ) {
-      fs.copyFileSync(path.join("dist", file), file.replace(".cjs", ".js"));
-    }
+    fs.copyFileSync(path.join("dist", file), file.replace(".cjs", ".js"));
   }
-  console.log("Type declaration files copied to root directory.");
 });
+
+/**
+ * Selects the files that are in the bundle that need to be copied to the root
+ * in order to satisfy Node 10 support. This is necessary because the attw
+ * package is not able to opt out of checking the Node 10 compatibility,
+ * so we just do this to satisfy it.
+ *
+ * In general, no one should want Node 10 support - this is a library intended
+ * to work in a browser environment.
+ */
+function isDesiredSourceFile(file: string) {
+  return (
+    file.endsWith("handler.d.ts") ||
+    file.endsWith("handler.cjs") ||
+    (file.endsWith(".d.ts") && file.includes("types-"))
+  );
+}
+
+function isCopiedTargetFile(file: string) {
+  return isDesiredSourceFile(file) || file.endsWith("handler.js");
+}

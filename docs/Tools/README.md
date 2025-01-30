0a813b17a6c3b4556c3eff4f8660728f3bfbcebb
Tom Hicks <tom.hicks@me.com>
2025-01-30 19:18:27 +0100

    Improve documentation

--- docs/Tools/README.md
+++ docs/Tools/README.md
@@ -2,6 +2,23 @@
 
 The Tools part allows you to let users draw elements on the map.
 
+## Example
+
+```ts
+// Change the settings for the line tool
+felt.setToolSettings({
+  tool: "line",
+  strokeWidth: 8,
+  color: "#448C2A"
+});
+
+// Start using the line tool
+felt.setTool("line");
+
+// Put down the tool
+felt.setTool(null);
+```
+
 ## Interfaces
 
 * [PinToolSettings](PinToolSettings.md)
--- src/modules/tools/index.ts
+++ src/modules/tools/index.ts
@@ -1,6 +1,22 @@
 /**
  * The Tools part allows you to let users draw elements on the map.
  *
+ * @example
+ * ```ts
+ * // Change the settings for the line tool
+ * felt.setToolSettings({
+ *   tool: "line",
+ *   strokeWidth: 8,
+ *   color: "#448C2A"
+ * });
+ *
+ * // Start using the line tool
+ * felt.setTool("line");
+ *
+ * // Put down the tool
+ * felt.setTool(null);
+ * ```
+ *
  * @module Tools
  */
 export type {

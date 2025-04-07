1019bd940f1a2ee5b9664bbb24b3c0957b6ac063
Tom Hicks <tom.hicks@me.com>
2025-04-07 12:19:48 +0200

    Ensure bundle is a single file by inspecting content

--- scripts/check-client-bundle-contents.js
+++ scripts/check-client-bundle-contents.js
@@ -11,25 +11,40 @@ const distPath = path.join(__dirname, "..", "dist");
 const clientFiles = fs.readdirSync(distPath).filter(file => /client\.c?js$/.test(file));
 
 // ensure we're not accidentally depending on zod (or anything else we shouldn't be) for our client bundle
-const disallowedStrings = ["zod"];
+const disalloweds = [
+  {
+    name: "import statement",
+    match: () => /import\s*{.*}\s*from\s*".*";/g,
+    highlight: () => /(.{0,20}import\s*{.*}\s*from\s*".*";.{0,20})/g,
+    description: "The client bundle should not contain import statements. This probably means you have something shared between the client and server that shouldn't be."
+  },
+  {
+    name: "zod",
+    match: () => /zod/g,
+    highlight: () => /(.{0,20}zod.{0,20})/g,
+    description: "The client bundle should not include zod. Please check your build configuration to ensure that the client bundle isn't importing anything it shouldn't be."
+  },
+];
 let failed = false;
 
 for (const file of clientFiles) {
   const filePath = path.join(distPath, file);
   const content = fs.readFileSync(filePath, "utf8");
 
-  for (const stringToFind of disallowedStrings) {
-    const stringToFindRegex = new RegExp(`(.{0,20}${stringToFind}.{0,20})`, "g");
-    if (content.includes(stringToFind)) {
-      const occurrences = content.match(stringToFindRegex);
+  for (const {name, match, highlight, description} of disalloweds) {
+    const matchRegex = match();
+    const highlightRegex = highlight();
+    
+    if (content.match(matchRegex)) {
+      const occurrences = content.match(highlightRegex);
       if (occurrences) {
-        console.error(`❌ Error: '${stringToFind}' found in ${filePath}`);
+        console.error(`❌ Error: '${name}' found in ${filePath}`);
         console.error("\nOccurrences:");
         occurrences.forEach(occurrence => {
-          const highlightedOccurrence = occurrence.replace(stringToFind, `\x1b[31m${stringToFind}\x1b[0m`);
+          const highlightedOccurrence = occurrence.replace(matchRegex, `\x1b[31m$&\x1b[0m`);
           console.error(`  ${highlightedOccurrence}`);
         });
-        console.error(`\nℹ️ The client bundle should not include ${stringToFind}. Please check your build configuration to ensure that the client bundle isn't importing anything it shouldn't be.`);
+        console.error(`\nℹ️ ${description}`);
         failed = true;
       }
     }
